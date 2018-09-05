3e6a4a37bcc28ebcc7bad1e1832bdcbd323db897
Vroni Fuchsberger <vronifuchsberger@gmail.com>
2018-09-05 09:38:02 +0200

    generate QR code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const {spawn, exec} = require('child_process');
 const path = require('path');
 const terminate = require('terminate');
 let win;
+let win2;
+const networkInterfaces = require('os').networkInterfaces();
 
 // create-react-app dev server starten
 const CRAprocess = spawn('yarn', ['start'], {
@@ -19,11 +21,26 @@ CRAprocess.stdout.on('data', data => {
   // when CRA has compiled everything, load URL in window
   if (String(data).trim() === 'Compiled successfully!') {
     win.loadURL('http://localhost:3000');
+    win2.loadURL(
+      `http://localhost:3000/qrCode.html?url=http://${getMyIP()}:8081/index.html`
+    );
   }
 });
 
+function getMyIP() {
+  for (let k in networkInterfaces) {
+    const inter = networkInterfaces[k];
+    for (let j in inter) {
+      if (inter[j].family === 'IPv4' && !inter[j].internal) {
+        return inter[j].address;
+      }
+    }
+  }
+}
+
 app.on('ready', () => {
   win = new BrowserWindow({width: 800, height: 600});
+  win2 = new BrowserWindow({width: 320, height: 360});
 });
 
 app.on('before-quit', e => {
