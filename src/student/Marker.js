c7c30173ce0a2e4957190be53cbf3b416522f08b
Vroni Fuchsberger <vronifuchsberger@gmail.com>
2018-11-12 15:52:02 +0100

    marker animation + scaling

--- src/student/Marker.js
+++ src/student/Marker.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import {View, NativeModules} from 'react-360';
+import {View, NativeModules, Animated} from 'react-360';
 import {connect} from './Store';
 import Entity from 'Entity';
 import AmbientLight from 'AmbientLight';
+import {Easing} from 'react-native';
+
+const AnimatedEntity = Animated.createAnimatedComponent(Entity);
 
 class Marker extends React.Component {
+  state = {
+    rotateAnim: new Animated.Value(0),
+  };
+
+  componentDidMount() {
+    this.startAnimation();
+  }
+
+  startAnimation() {
+    this.state.rotateAnim.setValue(0);
+    Animated.timing(this.state.rotateAnim, {
+      toValue: 1,
+      duration: 2000,
+      easing: Easing.linear,
+    }).start(() => {
+      this.startAnimation();
+    });
+  }
+
   render() {
     const hostname = NativeModules.HostnameModule.hostname;
     return this.props.markers ? (
       <View>
         <AmbientLight intensity={1.0} color={'#ffffff'} />
         {this.props.markers.map((position, i) => (
-          <Entity
+          <AnimatedEntity
             key={i}
             style={[
               {
@@ -19,13 +41,18 @@ class Marker extends React.Component {
                   {
                     translate: position.map(x => x * 800),
                   },
+                  {scale: [15, 15, 15]},
+                  {
+                    rotateY: this.state.rotateAnim.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: ['0deg', '360deg'],
+                    }),
+                  },
                 ],
               },
             ]}
             source={{
-              //obj: `http://localhost:8082/uploads/spider/Only_Spider_with_Animations_Export.obj`,
-              //mtl: `http://localhost:8082/uploads/spider/Only_Spider_with_Animations_Export.mtl`,
-              gltf2: `http://${hostname}:8082/uploads/cup.glb`,
+              gltf2: `http://${hostname}:8082/marker/marker1.glb`,
             }}
           />
         ))}
